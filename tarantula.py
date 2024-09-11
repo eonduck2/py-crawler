47bb85b34596e5f385bee86ba5838a21a9fb0638
이종수 <whdtnxd@gmail.com>
2024-09-11 14:14:37 +0900

    chore| -ㅅ-

--- tarantula.py
+++ tarantula.py
@@ -11,17 +11,17 @@ import modules.url.query_generator
 import modules.video
 import modules.video.crawl_videos 
 import static
-import static.goliath_tarantula_static
+import static.tarantula_static
 
 
-url = modules.url.query_generator.query_generator(input(static.goliath_tarantula_static.system_input_title))
+url = modules.url.query_generator.query_generator(input(static.tarantula_static.system_input_title))
 
 driver = modules.driver.get_driver.get_driver()
 
 driver.get(url)
 
 title = driver.title
-print(static.goliath_tarantula_static.youtube_title_prefix_message, title)
+print(static.tarantula_static.youtube_title_prefix_message, title)
 
 crawled_videos = modules.settings.crawl_setter.crawl_setter()
 
@@ -32,11 +32,11 @@ try:
             modules.browser_treader.browser_treader(driver)
 
         if modules.check.result_checker.result_checker(driver):
-            print(static.goliath_tarantula_static.no_more_result_kill_task_message)
+            print(static.tarantula_static.no_more_result_kill_task_message)
             break
 
 
 except KeyboardInterrupt:
-    print(static.goliath_tarantula_static.kill_task_crawling_message)
+    print(static.tarantula_static.kill_task_crawling_message)
 finally:
     driver.quit()
